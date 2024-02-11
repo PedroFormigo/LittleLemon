89307b8fda79cf1fd60db104cc9aef945a9c73b5
Pedro Formigo <pedroformigo@MacBook-Air-2.lan>
2024-02-11 01:14:59 +0000

    added models

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    id = models.PositiveIntegerField(max_length=11, primary_key=True)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(max_length=6)
+    booking_date = models.DateField()
+
+class Menu(models.Model):
+    id = models.PositiveIntegerField(max_length=5, primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.PositiveIntegerField(max_length=5)
