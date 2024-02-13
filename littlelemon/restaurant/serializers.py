39155093b532bd7847a867b8963f67d8988f1c00
Pedro Formigo <pedroformigo@MacBook-Air-2.lan>
2024-02-13 22:13:13 +0000

    booking api done

--- littlelemon/restaurant/serializers.py
+++ littlelemon/restaurant/serializers.py
@@ -1,7 +1,13 @@
 from .models import Menu
+from .models import Restaurant
 from rest_framework import serializers
 
 class MenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
-        fields = ['title', 'price', 'inventory']
+        fields = '__all__'
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = '__all__'
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,7 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework import generics
+from rest_framework import viewsets
 from .models import Menu
+from .models import Restaurant
 from .serializers import MenuSerializer
+from .serializers import BookingSerializer
 from django.shortcuts import render
 
 # Create your views here.
@@ -15,3 +18,7 @@ class MenuItemView(generics.ListCreateAPIView):
 class SingelMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Restaurant.objects.all()
+    serializer_class = BookingSerializer
