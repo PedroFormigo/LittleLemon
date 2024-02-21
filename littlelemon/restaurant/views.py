3333184d99377747f83dd2250894c701730bc2ec
Pedro Formigo <pedroformigo@MacBook-Air-2.lan>
2024-02-21 00:13:15 +0000

    Token authentication

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -6,6 +6,8 @@ from .models import Restaurant
 from .serializers import MenuSerializer
 from .serializers import BookingSerializer
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
 
 # Create your views here.
 def index(request):
@@ -19,6 +21,7 @@ class SingelMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
+@permission_classes([IsAuthenticated])
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Restaurant.objects.all()
     serializer_class = BookingSerializer
