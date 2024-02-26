b4b25b1058f1ceb10d5a7bc1665ef80f0320ecdd
Pedro Formigo <pedroformigo@MacBook-Air-2.lan>
2024-02-26 20:49:40 +0000

    final submision

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -10,3 +10,6 @@ class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.PositiveIntegerField(max_length=5)
+
+    def __str__(self):
+        return '{self.title}: {str(self.price)}'
